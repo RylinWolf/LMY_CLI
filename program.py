d9bcc29dd571d4b858a5bcd1ad2b4164f9fa1aaf
linexsong <1971257026@qq.com>
2023-11-16 12:53:31 +0800

    [doc] 部分修改

--- program.py
+++ program.py
@@ -154,8 +154,7 @@ def main():
         if main_func != "1":
             continue
 
-        # user_info = login(input("请输入账号: "), input("请输入密码: "))
-        user_info = login("13203549809", "woshiBlu.")
+        user_info = login(input("请输入账号: "), input("请输入密码: "))
         if not user_info["status"]:
             print("==========提示信息==========")
             print("登录失败! " + user_info["errorMessage"])
